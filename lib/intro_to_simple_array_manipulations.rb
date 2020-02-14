eea57fdbac15da5a406302e375cb0f8df3f7ed81
Natalie So <nataliesotszyui@gmail.com>
2020-02-14 22:30:38 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  a = array.pop()
+  return a
+end
+def pop_with_args(array)
+  a = array.pop(2)
+  return a
+end
+
+def using_shift(array)
+  a = array.shift()
+  return a
+end
+
+
+
+
+
